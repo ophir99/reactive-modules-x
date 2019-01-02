8cb3d3c8125a72f80efe292ac27b312764bcf465
kavya.pl <kavya.pl@gmail.com>
2019-01-02 21:22:30 +0530

    Form Arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormArray } from "@angular/forms";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -21,7 +21,29 @@ export class AppComponent {
       parents: new FormGroup({
         mother: new FormControl(),
         father: new FormControl()
-      })
+      }),
+      habits: new FormArray([]),
+      eduDetails: new FormArray([])
     });
+  }
+
+  pushHabitControls() {
+    const fc = new FormControl();
+    this.userRegistrationForm.get("habits").push(fc);
+  }
+
+  removeHabitFormControl(index) {
+    this.userRegistrationForm.get("habits").removeAt(index);
+  }
+  addEducationDetailsFG() {
+    const eduFG = new FormGroup({
+      course_name: new FormControl(),
+      institution: new FormControl(),
+      yearOfStarting: new FormControl(),
+      yearOfE: new FormControl(),
+      aggregate: new FormControl()
+    });
+
+    this.userRegistrationForm.get("eduDetails").push(eduFG);
   }
 }
